c66eaafae3630e277d0e3da525321c19d7b3b721
Andrés Zibula <30576177+andres-zibula@users.noreply.github.com>
2021-01-18 13:25:15 -0300

    renames & float types

--- Core/Inc/scara.h
+++ Core/Inc/scara.h
@@ -10,29 +10,32 @@
 
 #include "utils.h"
 
-#define S1_POS_X 0
-#define S1_POS_Y 0
+#define M1_POS_X .0f
+#define M1_POS_Y .0f
 
-#define S2_POS_X 50
-#define S2_POS_Y 0
+#define M2_POS_X 50.0f
+#define M2_POS_Y .0f
 
-#define ARM_LEN_1 75
-#define ARM_LEN_2 100
-#define ARM_LEN_3 35
+#define ARM_LEN_1 75.0f
+#define ARM_LEN_2 100.0f
+#define ARM_LEN_3 35.0f
 
 #define LIFT_HEIGHT_ANGLE 90
 #define BASE_HEIGHT_ANGLE 40
 
 #define STEPS_PER_MM 1.0f
-#define CIRCLE_PRECISION 0.05f
+#define CIRCLE_PRECISION 0.10f
 #define MS_PER_DEG 3.0f //1.7
 
 #define REDUCTION_RATIO 1.4f
+#define MICRO_STEPPING_DIV 32
 
 extern TIM_HandleTypeDef *htim;
 extern bool lifted;
 extern float actual_x;
 extern float actual_y;
+extern float m1_angle;
+extern float m2_angle;
 
 void lift();
 
--- Core/Src/scara.c
+++ Core/Src/scara.c
@@ -10,13 +10,15 @@
 bool lifted = false;
 float actual_x = .0f;
 float actual_y = .0f;
+float m1_angle = 90.0f;
+float m2_angle = 90.0f;
 
 void lift()
 {
     servo_write(htim, LIFT_HEIGHT_ANGLE);
 }
 
-void lift()
+void put_down()
 {
     servo_write(htim, BASE_HEIGHT_ANGLE);
 }
@@ -30,7 +32,7 @@ void draw_line(float x1, float y1, float x2, float y2, bool without_lifting)
   
   float dx = x2 - x1;
   float dy = y2 - y1;  
-  float c = round(STEPS_PER_MM * sqrt(dx*dx + dy*dy));
+  float c = roundf(STEPS_PER_MM * sqrtf(dx*dx + dy*dy));
   
   for(int i = 0; i <= c; i++)
   {
@@ -49,7 +51,7 @@ void draw_circle(float x, float y, float radius)
   if(lifted)
     put_down();
   
-  for(float r = CIRCLE_PRECISION; r <= M_PI*2; r += CIRCLE_PRECISION)
+  for(float r = CIRCLE_PRECISION; r <= M_PI*2.0f; r += CIRCLE_PRECISION)
   {
     //draw_line(actual_x, actual_y, x+cos(r)*radius, y+sin(r)*radius, true);
     go_to(x+cos(r)*radius, y+sin(r)*radius);
@@ -64,29 +66,29 @@ void go_to(float x, float y)
     return;
 
 
-  float L4 = cosine_side_rule(M_PI - M_PI/4.0, ARM_LEN_2, ARM_LEN_3);
+  float L4 = cosine_side_rule(M_PI - M_PI/4.0f, ARM_LEN_2, ARM_LEN_3);
   float epsilon = cosine_angle_rule(L4, ARM_LEN_2, ARM_LEN_3);
-  float d3 = pitagoras(S2_POS_X - x, y - S2_POS_Y);
-  float theta2 = atan2(y, (S2_POS_X - x)) + cosine_angle_rule(d3, L4, ARM_LEN_1);
+  float d3 = pitagoras(M2_POS_X - x, y - M2_POS_Y);
+  float theta2 = atan2f(y, (M2_POS_X - x)) + cosine_angle_rule(d3, L4, ARM_LEN_1);
   
-  float x4 = S2_POS_X + ARM_LEN_1*cos(M_PI - theta2);
-  float y4 = S2_POS_Y + ARM_LEN_1*sin(M_PI - theta2);
+  float x4 = M2_POS_X + ARM_LEN_1*cosf(M_PI - theta2);
+  float y4 = M2_POS_Y + ARM_LEN_1*sinf(M_PI - theta2);
   
-  float delta = atan2((x4-x), (y-y4));
+  float delta = atan2f((x4-x), (y-y4));
   
-  float x1 = x + ARM_LEN_3*sin(delta-epsilon);
-  float y1 = y - ARM_LEN_3*cos(delta-epsilon);
+  float x1 = x + ARM_LEN_3*sinf(delta-epsilon);
+  float y1 = y - ARM_LEN_3*cosf(delta-epsilon);
   
-  float d1 = pitagoras(x1 - S1_POS_X, y1 - S1_POS_Y);
-  float theta1 = atan2((y1 - S1_POS_Y), (x1 - S1_POS_X)) + cosine_angle_rule(d1, ARM_LEN_2, ARM_LEN_1);
+  float d1 = pitagoras(x1 - M1_POS_X, y1 - M1_POS_Y);
+  float theta1 = atan2f((y1 - M1_POS_Y), (x1 - M1_POS_X)) + cosine_angle_rule(d1, ARM_LEN_2, ARM_LEN_1);
 
-  float s1deg = rad_to_deg(theta1);
-  float s2deg = rad_to_deg(M_PI - theta2);
+  float m1deg = rad_to_deg(theta1);
+  float m2deg = rad_to_deg(M_PI - theta2);
    
-  servo1.write(s1deg + S1_OFFSET);
-  servo2.write(s2deg + S2_OFFSET);
+  servo1.write(m1deg + S1_OFFSET);
+  servo2.write(m2deg + S2_OFFSET);
   
-  float max_dist = max(abs(actual_s1_deg - s1deg), abs(actual_s2_deg - s2deg));
+  float max_dist = max(abs(actual_s1_deg - m1deg), abs(actual_s2_deg - m2deg));
   int delay_servo = ceil(MS_PER_DEG*ceil(max_dist));
 
   if (delay_servo < 10)
@@ -94,8 +96,8 @@ void go_to(float x, float y)
   
   actual_x = x;
   actual_y = y;
-  actual_s1_deg = s1deg;
-  actual_s2_deg = s2deg;
+  actual_s1_deg = m1deg;
+  actual_s2_deg = m2deg;
   
   delay(delay_servo);
 }
